6176f637bb1688657a1ded83b19387817887bafb
Suganya <suganprakash0621@gmail.com>
2021-02-12 14:53:52 -0600

    created table

--- raterprojectapi/models/games.py
+++ raterprojectapi/models/games.py
@@ -9,4 +9,4 @@ class Games(models.Model):
     releaseYear= models.IntegerField()
     timeToPlay= models.CharField(max_length=20)
     ageRec= models.IntegerField()
-    categoryId= models.ForeignKey("Category",on_delete=models.CASCADE)
+    
