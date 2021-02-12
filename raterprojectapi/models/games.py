0e971a3b8bf860edc861d08d84dba84ab6b307ac
Suganya <suganprakash0621@gmail.com>
2021-02-12 14:44:02 -0600

    changed the ClassNAme

--- raterprojectapi/models/games.py
+++ raterprojectapi/models/games.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-class Player(models.Model):
+class Games(models.Model):
 
     title= models.CharField(max_length=50)
     description= models.CharField(max_length=30)
     designer= models.CharField(max_length=30)
+    Number_of_players = models.IntegerField()
     releaseYear= models.IntegerField()
     timeToPlay= models.CharField(max_length=20)
     ageRec= models.IntegerField()
