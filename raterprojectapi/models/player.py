49b98826d6877feded7df30a5c3de69c00b0a473
Suganya <suganprakash0621@gmail.com>
2021-02-12 14:21:00 -0600

    coded for models

--- raterprojectapi/models/player.py
+++ raterprojectapi/models/player.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class Player(models.Model):
 
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    
+    bio = models.CharField(max_count=75)
